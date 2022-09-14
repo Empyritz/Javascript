792e6e51c1947e039d9f565efb38d08f11eb7c0a
Fabian NP <fabian.nava.pereda@gmail.com>
2022-09-14 13:19:37 -0500

    Making a conter with events onclick and functionsthat restar the counter and put the reslut in the DOM

---
+++ index.js
@@ -0,0 +1,22 @@
+let countEl = document.getElementById
+("count-el");
+let saveEl = document.getElementById
+("save-el");
+let count = 0;
+
+
+
+function increment () {
+    count += 1;
+    countEl.innerText = count
+    console.log(count)
+};
+
+function save() {
+    let countStr = count + " - ";
+    // saveEl.innerText += countStr;
+    saveEl.textContent += countStr;
+    console.log(count);
+    count = 0;
+    countEl.innerText = count
+ };
