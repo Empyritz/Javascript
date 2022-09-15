c14f72c505ff3f5640e64ec0a8777edbff017f72
Fabian NP <fabian.nava.pereda@gmail.com>
2022-09-15 16:03:27 -0500

    Making a BlackJacj Game using random numbers and printing on screen the cards and the result of rhe cards and saying to the player if want a card if he lost or if he won

--- main.js
+++ main.js
@@ -1,15 +1,65 @@
-let firstCard = 6;
-let secondCard = 9;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
 let sum = firstCard + secondCard;
+let cards = [firstCard, secondCard];
+let message = ""
+let isAlive = true
+let hasBlackjack = false
+
+
+let messageEl = document.querySelector("#message-el")
+let startBtn = document.querySelector("#buttonStart")
+let newBtn = document.querySelector("#buttonNew")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
+
+
+startBtn.addEventListener("click", startGame)
+newBtn.addEventListener("click", newCard)
+
+
+function getRandomCard () {
+    let randomNumber = Math.floor(Math.random()*13) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber == 1) {
+        return 11
+    } else {
+        return randomNumber;
+    }
+}
+
 
 function startGame () {
+    renderGame()
+}
+
+function renderGame () {
     if (sum < 21) {
-        console.log("Do you want another card")
+        message = "Do you want another card?"
     } else if (sum === 21) {
-        console.log("Wohoo! You`ve got Blackjack")
+        message = "Wohoo! You`ve got Blackjack"
+        hasBlackjack = true
     } else {
-        console.log("You`re out of the Game")
+        message = "You`re out of the Game"
+        isAlive = false
+    };
+
+    cardsEl.textContent = "Cards: " ;
+
+    for (i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
     }
+    sumEl.textContent = "Sum: " + sum;
+    messageEl.textContent = message;
+
+};
+
+function newCard () {
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    renderGame()
 }
 
 
