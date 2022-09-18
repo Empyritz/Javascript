6c4f1f5df3b6611a7a97584108e3b2a66537deab
empyritz <fabian.nava.pereda@gmail.com>
2022-09-17 22:20:59 -0500

    Adding the button add tab to get the currentWindow and sava the url, and addied a button delete to clear all the localStorage and list of tabs printed on screen

--- main.js
+++ main.js
@@ -1,30 +1,62 @@
 const inputEl = document.querySelector("#input-el")
 const inputBtn = document.querySelector("#input-btn")
+const deleteBtn = document.querySelector("#delete-btn")
+const tabBtn = document.querySelector("#tab-btn")
 const ulEl = document.querySelector("#ul-el")
-
-const myLeads = []
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads"))
 const link = "https://"
-
+let myLeads = []
+//const tabs = [{url: "https:www.linkedin.com"}]
 
 inputBtn.addEventListener("click", saveLead)
+deleteBtn.addEventListener("dblclick", deleteLead)
+tabBtn.addEventListener("click", tabF)
 
-function saveLead() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
 }
 
-function renderLeads () {
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++){
-        listItems += `<li><a href = "${link}${myLeads[i]}" target = "_blanck">${myLeads[i]}</li></a>`
+function tabF() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+        console.log(myLeads)
+    }) 
+}
+//var listItems = ""
 
+function render (arr) {
+    let listItems =""
+    for (let i = 0; i < arr.length; i++){
+        listItems += `<li><a href = "${link}${arr[i]}" target = "_blanck">${myLeads[i]}</li></a>`
+        console.log(listItems)
     }
+    console.log(listItems)
+
     ulEl.innerHTML = listItems
     //const li = document.createElement("li")
     //li.textContent = myLeads[i]
     //ulEl.append(li)
 }
+
+function deleteLead (arr){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+}
+function saveLead() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+
+    console.log( localStorage.getItem("myLeads"))
+}
+
+
 
 
 
